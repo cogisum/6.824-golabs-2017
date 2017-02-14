f4c9dd00fae90c7e26309911a24e2f6d41ee0fc6
let <a@b.c>
2017-02-14 17:57:16 +0800

    pass handling worker failures

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -3,6 +3,7 @@ package mapreduce
 import (
 	"fmt"
 	"sync"
+    "time"
 )
 
 //
@@ -35,9 +36,38 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 	//
 	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
 	//
+    todoch := make(chan int)
+    go func() {
+        for taskNumber := 0; taskNumber < ntasks; taskNumber++ {
+            todoch <- taskNumber
+        }
+    }()
+
 	wg := sync.WaitGroup{}
-    wg.Add(ntasks)
-	for taskNumber := 0; taskNumber < ntasks; taskNumber++ {
+    count := ntasks // num of tasks remaining to run
+    mu := sync.Mutex{}
+    for {
+        mu.Lock()
+        cnt := count
+        if count > 0 {
+            count--
+        }
+        mu.Unlock()
+        fmt.Printf("remain %v\n", cnt)
+        if cnt == 0 {
+            fmt.Println("in cnt == 0")
+            wg.Wait()
+            fmt.Println("after wg.wait", wg)
+            mu.Lock()
+            cnt := count
+            mu.Unlock()
+            if cnt == 0 {
+                break
+            } else {
+                continue
+            }
+        }
+        taskNumber := <-todoch
 		args := DoTaskArgs{}
 		args.JobName = jobName
 		args.Phase = phase
@@ -47,14 +77,33 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 		args.TaskNumber = taskNumber
 		args.NumOtherPhase = n_other
 		worker := <-registerChan
-		go func() {
-			call(worker, "Worker.DoTask", args, new(struct{}))
+        wg.Add(1)
+		go func(id int) {
+            state := call(worker, "Worker.DoTask", args, new(struct{}))
             // NOTE: if put wg.Done() after registerChan <- worker
-            // it won't complete, perhaps because the channel is full?
-			wg.Done()
-            registerChan <- worker
-		}()
+            // it won't complete, because registerChan is unbuffered,
+            // send to this channel will succeed only if there is a
+            // receiver, so wg.Done() in the last thread corresponding to 
+            // the two tasks will never be executed.
+
+            // tricky, if put wg.Done directly after state, then if the task
+            // failed, the main thread may not see the updated count
+            if !state {
+                mu.Lock()
+                count++
+                mu.Unlock()
+                todoch <- id
+
+            }
+            wg.Done()
+			if state {
+                fmt.Printf("done task %v\n", id)
+                registerChan <- worker
+            }
+            fmt.Printf("finish task %v\n", id)
+		}(taskNumber)
 	}
-	wg.Wait()
+    time.Sleep(2 * time.Second)
+    fmt.Println("waitgroup is ", wg)
 	fmt.Printf("Schedule: %v phase done\n", phase)
 }
